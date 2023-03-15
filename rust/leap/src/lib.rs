0455ab331666b92f7f5e8d9c8d32040a25cc637b
bontaramsonta <18cs113@gmail.com>
2023-03-16 02:56:26 +0530

    solved: gigaseconds

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -12,11 +12,11 @@ pub fn is_leap_year(year: u64) -> bool {
 }
 
 // match style
-pub fn is_leap_year(year: u64) -> bool {
-    match year {
-        x if x % 400 == 0 => true,
-        x if x % 100 == 0 => false,
-        x if x % 4 == 0 => true,
-        _ => false,
-    }
-}
+// pub fn is_leap_year(year: u64) -> bool {
+//     match year {
+//         x if x % 400 == 0 => true,
+//         x if x % 100 == 0 => false,
+//         x if x % 4 == 0 => true,
+//         _ => false,
+//     }
+// }
